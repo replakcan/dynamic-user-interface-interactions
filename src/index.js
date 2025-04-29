3478bb9ffba995dee0589818e732bc49de53c4cd
replakcan <alper.makcan@gmail.com>
2025-04-29 18:42:41 +0300

    Demonstrates dropdown button

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
+import dropdownBtn from './component/dropdown-btn'
 import './reset.css'
 import './styles.css'
 
 console.log('selamin hello')
+
+const nav = document.querySelector('nav')
+
+dropdownBtn('dropdown button', 'dropdown-menu', nav)
